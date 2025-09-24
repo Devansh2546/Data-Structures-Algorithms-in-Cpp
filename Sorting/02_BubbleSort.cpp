d37b130ad6b8f0c9f69263212b0129dbd100f370
Devansh Thakkar <devansht2546@gmail.com>
2025-09-24 22:54:52 +0530

    Bubble Sort

---
+++ Sorting/02_BubbleSort.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++) // n-i-1 because last i elements will be automatically sorted
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                // Alternavtive option
+                // swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+    cout << "Sorted array using Bubble sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
